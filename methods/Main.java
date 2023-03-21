a515961f1d373c75750a1b0f4f78a76c30460f94
Christopher Jimenez <chrisjimenez.fba@gmail.com>
2023-03-20 23:38:14 -0700

    adding methods folder

---
+++ methods/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+
+ public static void main(String []args){
+	// This method will be using the Scanner class 
+	System.out.println("Enter in two integers");
+	Scanner sc = new Scanner(System.in); 
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+
+	
+	}
+
+	public static int total(int num1,int num2){
+		int sum;
+		sum = num1 + num2;
+		return sum;
+
+	}
+	
+}
